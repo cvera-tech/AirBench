c2106616b5c2207adf2d0d7e375a7e8723993073
jbvera <johnbenedictv@gmail.com>
2019-09-30 15:15:27 -0400

    Set up Unity

--- src/AirBench/App_Start/UnityConfig.cs
+++ src/AirBench/App_Start/UnityConfig.cs
@@ -1,47 +1,21 @@
-using System;
-
+using System.Web.Http;
+using System.Web.Mvc;
 using Unity;
+using Mvc5Resolver = Unity.AspNet.Mvc.UnityDependencyResolver;
+using ApiResolver = Unity.AspNet.WebApi.UnityDependencyResolver;
 
 namespace AirBench
 {
-    /// <summary>
-    /// Specifies the Unity configuration for the main container.
-    /// </summary>
     public static class UnityConfig
     {
-        #region Unity Container
-        private static Lazy<IUnityContainer> container =
-          new Lazy<IUnityContainer>(() =>
-          {
-              var container = new UnityContainer();
-              RegisterTypes(container);
-              return container;
-          });
-
-        /// <summary>
-        /// Configured Unity Container.
-        /// </summary>
-        public static IUnityContainer Container => container.Value;
-        #endregion
-
-        /// <summary>
-        /// Registers the type mappings with the Unity container.
-        /// </summary>
-        /// <param name="container">The unity container to configure.</param>
-        /// <remarks>
-        /// There is no need to register concrete types such as controllers or
-        /// API controllers (unless you want to change the defaults), as Unity
-        /// allows resolving a concrete type even if it was not previously
-        /// registered.
-        /// </remarks>
-        public static void RegisterTypes(IUnityContainer container)
+        public static void RegisterComponents()
         {
-            // NOTE: To load from web.config uncomment the line below.
-            // Make sure to add a Unity.Configuration to the using statements.
-            // container.LoadConfiguration();
+            var container = new UnityContainer();
 
-            // TODO: Register your type's mappings here.
-            // container.RegisterType<IProductRepository, ProductRepository>();
+            // e.g. container.RegisterType<ITestService, TestService>();
+
+            DependencyResolver.SetResolver(new Mvc5Resolver(container));
+            GlobalConfiguration.Configuration.DependencyResolver = new ApiResolver(container);
         }
     }
 }
