c543d8f8f22d15a9dcced456613c5d4c989784f3
jbvera <johnbenedictv@gmail.com>
2019-10-01 12:03:09 -0400

    Add index view to initialize database

---
+++ src/AirBench/Data/BenchInitializer.cs
@@ -0,0 +1,18 @@
+﻿using AirBench.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AirBench.Data
+{
+    public class BenchInitializer : DropCreateDatabaseAlways<BenchContext>
+    {
+        protected override void Seed(BenchContext context)
+        {
+            context.Benches.Add(new Bench() { Id = 1, Description = "Bench", Latitude = 10f, Longitude = 20f, NumberSeats = 4 });
+            base.Seed(context);
+        }
+    }
+}
