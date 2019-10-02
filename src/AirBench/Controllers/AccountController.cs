dd830cb56f967856731dd42faa93896455a8383a
jbvera <johnbenedictv@gmail.com>
2019-10-02 12:05:34 -0400

    Implement Account Login POST action

--- src/AirBench/Controllers/AccountController.cs
+++ src/AirBench/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using AirBench.Data.Repositories;
 using AirBench.Models.ViewModels;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace AirBench.Controllers
 {
@@ -16,7 +17,7 @@ namespace AirBench.Controllers
         [AllowAnonymous]
         public ActionResult Login()
         {
-            var tempPassword = BCrypt.Net.BCrypt.HashPassword("john");
+            //var tempPassword = BCrypt.Net.BCrypt.HashPassword("john");
             var viewModel = new AccountLoginViewModel();
             return View(viewModel);
         }
@@ -26,7 +27,21 @@ namespace AirBench.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Login(AccountLoginViewModel viewModel)
         {
-            var authenticated = _repo.Authenticate(viewModel.Username, viewModel.Password);
+            if (ModelState.IsValid &&
+                ModelState.IsValidField("Username") &&
+                ModelState.IsValidField("Password"))
+            {
+                var authenticated = _repo.Authenticate(viewModel.Username, viewModel.Password);
+                if (authenticated)
+                {
+                    FormsAuthentication.SetAuthCookie(viewModel.Username, false);
+                    return RedirectToAction("Index", "Bench");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid username or password.");
+                }
+            }
             return View(viewModel);
         }
     }
