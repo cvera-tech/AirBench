858fb023dc998226cad720fa4e23bcc862db35cb
jbvera <johnbenedictv@gmail.com>
2019-10-02 16:02:07 -0400

    Add add review client-side violation

---
+++ src/AirBench/Scripts/add-review-validation.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    const form = document.getElementById('AddReviewForm')
+    const description = document.getElementById('Description');
+    const rating = document.getElementById('Rating');
+    const submit = document.getElementById('Submit');
+
+    form.addEventListener('input', () => {
+        if (description.validity.valid && rating.validity.valid) {
+            submit.setAttribute('class', 'btn btn-primary');
+            submit.removeAttribute('disabled');
+        } else {
+            submit.setAttribute('class', 'btn btn-primary disabled');
+            submit.setAttribute('disabled', 'disabled');
+        }
+    });
+})()
