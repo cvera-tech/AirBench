b828dca5f13b853024fe78a992cdb1e65f270da6
jbvera <johnbenedictv@gmail.com>
2019-10-02 09:26:44 -0400

    Add Bench Review action

---
+++ src/AirBench/Models/ViewModels/ReviewAddViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace AirBench.Models.ViewModels
+{
+    public class ReviewAddViewModel
+    {
+        public Bench Bench { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+
+        public List<SelectListItem> RatingItems { get; private set; }
+
+        public ReviewAddViewModel()
+        {
+            RatingItems = new List<SelectListItem>();
+            for (int index = 5; index > 0; index -= 1)
+            {
+                RatingItems.Add(new SelectListItem()
+                {
+                    Value = index.ToString(),
+                    Text = index.ToString()
+                });
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"({Rating}: {Description}";
+        }
+    }
+}
